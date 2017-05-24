802fc13e999127b795a5698d4c6432999c893340
Lausbert <stephanlerner89@googlemail.com>
2017-05-24 17:45:04 +0200

    added Constants.swift

---
+++ ExermoteGoogleMachineLearningEngine/ExermoteMachineLearningEngine/ExermoteMachineLearningEngine/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Exermote
+//
+//  Created by Stephan Lerner on 30.10.16.
+//  Copyright © 2016 Stephan. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Device Data Constants
+
+let DEVICE_DATA = ["Device: xGravity",
+                   "Device: yGravity",
+                   "Device: zGravity",
+                   "Device: xAcceleration",
+                   "Device: yAcceleration",
+                   "Device: zAcceleration",
+                   "Device: pitch",
+                   "Device: roll",
+                   "Device: yaw",
+                   "Device: xRotationRate",
+                   "Device: yRotationRate",
+                   "Device: zRotationRate"]
+
