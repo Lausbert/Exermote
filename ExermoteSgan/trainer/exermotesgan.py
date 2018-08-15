9f81e5444cc771deabff1cebd08ca818e8d3aeb4
Lausbert <stephanlerner89@googlemail.com>
2018-08-15 09:43:22 +0200

    updated trainer

--- ExermoteSgan/trainer/exermotesgan.py
+++ ExermoteSgan/trainer/exermotesgan.py
@@ -88,67 +88,44 @@ class SGAN:
         self.baseline.compile(loss=["categorical_crossentropy"], optimizer=optimizer, metrics=["accuracy"])
 
     def __build_discriminator(self):
-
         model = Sequential()
-
         model.add(Conv1D(self.nodes_per_layer, self.filter_length, strides=2, activation="relu", input_shape=(self.timesteps, self.features)))
         model.add(Conv1D(self.nodes_per_layer, self.filter_length, strides=1, activation="relu"))
         model.add(LSTM(self.nodes_per_layer, return_sequences=True))
         model.add(LSTM(self.nodes_per_layer, return_sequences=False))
         model.add(Dropout(self.dropout))
-
         accelerations = Input(shape=(self.timesteps, self.features), name="accelerations")
         features = model(accelerations)
         valid = Dense(1, activation="sigmoid")(features)
         scores = Dense(self.num_classes + 1, activation="softmax", name="scores")(features)
-
         return Model(accelerations, [valid, scores])
 
     def __build_generator(self):
-
-        factor = 2
         model = Sequential()
-        model.add(Dense((((self.timesteps * factor) + 20) * self.features * 5), activation="relu", input_dim=self.generator_input_length))
-        print(model.output_shape)
-        model.add(Reshape(((self.timesteps * factor) + 20, self.features * 5)))
-        print(model.output_shape)
+        model.add(Dense((((self.timesteps * 2) + 20) * self.features * 5), activation="relu", input_dim=self.generator_input_length))
+        model.add(Reshape(((self.timesteps * 2) + 20, self.features * 5)))
         model.add(LSTM(self.features * 5, return_sequences=True))
-        print(model.output_shape)
         model.add(LSTM(self.features * 5, return_sequences=True))
-        print(model.output_shape)
-        model.add(Reshape(((self.timesteps * factor) + 20, self.features * 5, 1)))
-        print(model.output_shape)
+        model.add(Reshape(((self.timesteps * 2) + 20, self.features * 5, 1)))
         model.add(AveragePooling2D(pool_size=(1, 5)))
-        print(model.output_shape)
         model.add(Conv2D(128, kernel_size=(1, 3), padding="same", activation="relu"))
-        print(model.output_shape)
         model.add(Conv2D(64, kernel_size=(1, 3), padding="same", activation="relu"))
-        print(model.output_shape)
         model.add(Conv2D(1, kernel_size=3, padding="same", activation="tanh"))
-        print(model.output_shape)
-        model.add(AveragePooling2D(pool_size=(factor, 1)))
-        print(model.output_shape)
+        model.add(AveragePooling2D(pool_size=(2, 1)))
         model.add(Cropping2D(cropping=((5, 5), (0, 0))))
-        print(model.output_shape)
-
         noise = Input(shape=(self.generator_input_length,), name="noise")
         accelerations = Reshape((self.timesteps, self.features), name="accelerations")(model(noise))
-
         return Model(noise, accelerations)
 
     def __build_baseline(self):
-
         model = Sequential()
-
         model.add(Conv1D(self.nodes_per_layer, self.filter_length, strides=2, activation="relu", input_shape=(self.timesteps, self.features)))
         model.add(Conv1D(self.nodes_per_layer, self.filter_length, strides=1, activation="relu",))
         model.add(LSTM(self.nodes_per_layer, return_sequences=True))
         model.add(LSTM(self.nodes_per_layer, return_sequences=False))
         model.add(Dropout(self.dropout))
-
         accelerations = Input(shape=(self.timesteps, self.features), name="accelerations")
         scores = Dense(self.num_classes, activation="softmax", name="scores")(model(accelerations))
-
         return Model(accelerations, scores)
 
 
